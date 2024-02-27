db6a25ee2153610c452e1684d0cb627320a16312
VladislavFedorchuk <142826785+VladislavFedorchuk@users.noreply.github.com>
2024-02-27 10:22:01 +0200

    release

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useMemo, useEffect } from 'react';
+
+import Description from './components/Description';
+import Options from './components/Options';
+import Feedback from './components/Feedback';
+import Notification from './components/Notification';
+
+import './App.css';
+
+function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem('feedback'));
+
+    return (
+      savedFeedback || {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      }
+    );
+  });
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const addFeedbackHandler = (key) => {
+    setFeedback({ ...feedback, [key]: feedback[key] + 1 });
+  };
+
+  const resetFeedbackHandler = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = useMemo(() => {
+    const { good, neutral, bad } = feedback;
+    return good + neutral + bad;
+  }, [feedback]);
+
+  return (
+    <div className="App">
+      <Description />
+      <Options
+        addFeedback={addFeedbackHandler}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedbackHandler}
+      />
+      {!totalFeedback ? (
+        <Notification />
+      ) : (
+        <Feedback {...feedback} totalFeedback={totalFeedback} />
+      )}
+    </div>
+  );
+}
+
+export default App;
